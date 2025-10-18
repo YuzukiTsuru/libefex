7e00e57697759c6be26768142b0a61865668e7ff
YuzukiTsuru <gloomyghost@gloomyghost.com>
2025-10-18 22:19:00 +0800

    refactor: Update FEL references to EFEX in efex-cli.c

--- test_compile.c
+++
@@ -1,9 +0,0 @@
-#include "includes/libefex.h"
-#include "includes/efex-protocol.h"
-#include "includes/efex-payloads.h"
-
-int main() {
-    struct sunxi_efex_ctx_t ctx = {0};
-    printf("Testing libefex compilation\n");
-    return 0;
-}
