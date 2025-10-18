bdbe44dbcb630e03261ecee3346bc39fa82cb5dd
YuzukiTsuru <gloomyghost@gloomyghost.com>
2025-10-18 22:13:48 +0800

    refactor: Clean up compiler.h by consolidating packed definitions and warning suppressions

--- includes/compiler.h
+++ includes/compiler.h
@@ -6,12 +6,11 @@
 // Microsoft Visual C++ compiler
 #define EFEX_PACKED_BEGIN __pragma(pack(push, 1))
 #define EFEX_PACKED_END __pragma(pack(pop))
-#define EFEX_PACKED 
-// 抑制特定警告
-#pragma warning(disable: 4018) // 有符号/无符号不匹配比较
-#pragma warning(disable: 4267) // 从size_t转换到较小整数类型
-#pragma warning(disable: 4996) // 不安全的函数（如strcpy）
-#define _CRT_SECURE_NO_WARNINGS // 禁用CRT安全警告
+#define EFEX_PACKED
+#pragma warning(disable: 4018)
+#pragma warning(disable: 4267)
+#pragma warning(disable: 4996)
+#define _CRT_SECURE_NO_WARNINGS
 #else
 // GCC, Clang and other compilers
 #define EFEX_PACKED_BEGIN
